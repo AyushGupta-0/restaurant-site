18829ecddadff8eb8754498f0925fe679363b257
Ayush Gupta <118269589+AyushGupta-0@users.noreply.github.com>
2023-04-08 19:19:51 +0530

    adding individual items & buiding a Form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React,{ Fragment } from "react";
 import Header from "./component/LAYOUT/Header";
+import Meals from "./component/Meals/Meals";
+
+
 function App() {
   return (
     <Fragment>
       <Header/>
+      <main>
+        <Meals/>
+      </main>
     </Fragment>
   );
 }
