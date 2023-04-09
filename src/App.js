be8b783b9367b261ae4da92843f6b83784695a96
Ayush Gupta <118269589+AyushGupta-0@users.noreply.github.com>
2023-04-09 15:02:13 +0530

    managing cart state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React,{ Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./component/LAYOUT/Header";
 import Meals from "./component/Meals/Meals";
 import Cart from "./component/Cart/Cart";
 
-
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart/>
-      <Header/>
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler} />
       <main>
-        <Meals/>
+        <Meals />
       </main>
     </Fragment>
   );
--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -2,19 +2,27 @@ import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 
 const Cart = (props) => {
-  const cartitems = <ul className={classes['cart-items']}>{[
-    { id: "c1", name: "Sushi", amount: "2", price: "2.99" },
-  ].map((items) => <li>{items.name}</li>)}</ul>;
+  const cartitems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c1", name: "Sushi", amount: "2", price: "2.99" }].map(
+        (items) => (
+          <li>{items.name}</li>
+        )
+      )}
+    </ul>
+  );
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartitems}
       <div className={classes.total}>
         <span>Amount</span>
         <span>35.99</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
