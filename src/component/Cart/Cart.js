1efafc35776d94e52746e5be1b7daf25972b3353
Ayush Gupta <118269589+AyushGupta-0@users.noreply.github.com>
2023-04-13 14:46:07 +0530

    Adding and Removing Items from Cart

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,14 +1,34 @@
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
+import { useContext } from "react";
+import CartItem from "./CartItem";
+import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item , amount:1})
+  };
+
   const cartitems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: "2", price: "2.99" }].map(
-        (items) => (
-          <li>{items.name}</li>
-        )
-      )}
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null , item.id)}
+          onAdd={cartItemAddHandler.bind(null , item)}
+        />
+      ))}
     </ul>
   );
 
@@ -17,13 +37,13 @@ const Cart = (props) => {
       {cartitems}
       <div className={classes.total}>
         <span>Amount</span>
-        <span>35.99</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
--- src/component/LAYOUT/HeaderCartButton.js
+++ src/component/LAYOUT/HeaderCartButton.js
@@ -4,12 +4,11 @@ import { useContext } from "react";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
-
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber ,  item)=>{
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
-  } , 0 );
+  }, 0);
 
   return (
     <button className={classes.button} onClick={props.onClick}>
